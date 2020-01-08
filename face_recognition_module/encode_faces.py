f2a753aa7f8d5b9ad1508038a4193fab7c2f4578
sharif-42 <sharifulcsehstu@gmail.com>
2020-01-09 00:48:02 +0600

    face recognition module Progressing

--- face_recognition_module/encode_faces.py
+++ face_recognition_module/encode_faces.py
@@ -1,4 +1,14 @@
 import face_recognition
+import pickle
+import cv2
+import imutils
 
-image = face_recognition.load_image_file("faces_datasets/alan_grant/00000000.jpg")
-face_locations = face_recognition.face_locations(image)
+imagePath = "face_datasets/alan_grant/00000000.jpg"
+image = cv2.imread(imagePath)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+resized_image = imutils.resize(rgb, height=600,width=700)
+cv2.imshow('resized_image',resized_image)
+cv2.waitKey(0)
+
+# image = face_recognition.load_image_file("face_datasets/alan_grant/00000000.jpg")
+# face_locations = face_recognition.face_locations(image)
